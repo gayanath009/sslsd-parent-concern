5d88c0798dfad8315ea171310c1e00b4b8e2bd42
Gayanath Perera <gayanath009@yahoo.com>
2026-01-14 18:37:49 +0300

    env update

--- SupabaseClient.js
+++ SupabaseClient.js
@@ -1,7 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseanonkey=import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl,supabaseanonkey)
+const supabaseanonkey = import.meta.env.VITE_SUPABASE_ANON_KEY
+export const supabase = createClient(supabaseUrl, supabaseanonkey)
 
